ba263ff0e01985d30d0556ad7e26ee26d289d79b
leoguedesoliveira <leonardoguedes.dasilvaguedesol@gmail.com>
2025-01-24 18:55:31 -0300

    Iniciando projeto Spring Boot

---
+++ backend/gestao-escolar/src/test/java/com/lgtech/gestao_escolar/GestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgtech.gestao_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
